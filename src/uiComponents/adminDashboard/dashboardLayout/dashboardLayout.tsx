a5ae689b34b7c6a5e78ffefb27108ddc6d84e840
SuryaC333 <suryachakraborty9@gmail.com>
2025-04-01 21:18:43 +0530

    Dashboard layout-added icons and css changes

--- src/uiComponents/adminDashboard/dashboardLayout/dashboardLayout.tsx
+++ src/uiComponents/adminDashboard/dashboardLayout/dashboardLayout.tsx
@@ -2,6 +2,13 @@ import React, { useEffect } from "react";
 import { DashboardNavigation } from "../dashboardNavigation";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { AppConstants } from "../../../AppConstants";
+import AssignmentIndIcon from "@mui/icons-material/AssignmentInd";
+import { Color } from "../../../App.model";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import classes from "./dashboardLayout.module.css";
+import LightModeIcon from "@mui/icons-material/LightMode";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import ModeNightIcon from "@mui/icons-material/ModeNight";
 
 export const DashboardLayout: React.FC = () => {
   const location = useLocation();
@@ -21,11 +28,29 @@ export const DashboardLayout: React.FC = () => {
 
   return (
     <div className="d-flex">
-      <div className="col-md-2">
+      <div className={`${classes.sidenavContainer} col-md-2`}>
         <DashboardNavigation />
       </div>
       <div className="col-md-10">
-        <Outlet />
+        <div className={`${classes.menuAndOutletContainer} px-5 py-4`}>
+          <div className="d-flex justify-content-end">
+            <div className="d-flex col-md-6 justify-content-end">
+              <div className={classes.icon}>
+                <LightModeIcon fontSize="medium" color={Color.Warning} />
+              </div>
+              <div className={classes.icon}>
+                <NotificationsNoneIcon
+                  color={Color.Secondary}
+                  fontSize="medium"
+                />
+              </div>
+              <div className={classes.icon}>
+                <AssignmentIndIcon color={Color.Primary} fontSize="medium" />
+              </div>
+            </div>
+          </div>
+          <Outlet />
+        </div>
       </div>
     </div>
   );
